eefc5975ca2567b9070155d4642c4217bc3249aa
prit2596 <pritthakkar2.pt@gmail.com>
2017-10-30 01:28:32 +0530

    x

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 
 <?php
-  $host="localhost";
-  $user="root";
-  $pass="";
+  $host="us-cdbr-iron-east-05.cleardb.net";
+  $user="be3c88f89b6700";
+  $pass="d06bcc09";
   $db_name="attendance";
 
   $mysqli=new mysqli($host,$user,$pass,$db_name);
